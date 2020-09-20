100e3c845b06fe438c75b86e040a86c9416ef180
ainzzorl <ainzzorl@gmail.com>
2020-09-19 18:30:39 -0700

    Add new script to parse text with nltk

---
+++ data-scripts/import.py
@@ -0,0 +1,45 @@
+import os
+import nltk
+import json
+
+def process(lang, id):
+  with open(f"./data-scripts/in/{id}/{lang}.txt") as file:
+    data = file.read()
+
+    title, body = data.split("\n", maxsplit=1)
+
+    if lang == 'en':
+      tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    elif lang == 'es':
+      tokenizer = nltk.data.load('tokenizers/punkt/spanish.pickle')
+    else:
+      raise f"Unsupported language: {lang}"
+
+    sentences = tokenizer.tokenize(body)
+
+  return {
+    'title': title,
+    'sentences': sentences
+  }
+
+for id in os.listdir('./data-scripts/in/'):
+  print(id)
+  en = process('en', id)
+  es = process('es', id)
+
+  if len(en['sentences']) != len(es['sentences']):
+    minlen = min(len(en['sentences']), len(es['sentences']))
+    for i in range(minlen):
+      print(f"{en['sentences'][i]} <=> {es['sentences'][i]}\n\n")
+    end
+    raise f"Length mismatch: en={len(en['sentences'])} vs es={len(es['sentences'])}"
+  else:
+    print("Everything's fine")
+
+  final = {
+    'en': en,
+    'es': es
+  }
+
+  with open(f"public/assets/data/texts/{id}.json", 'w') as outfile:
+    json.dump(final, outfile, indent=2)
