c91619905b61bd208de1b5fef2c31e674f00f500
ainzzorl <ainzzorl@gmail.com>
2020-12-06 20:16:27 -0800

    Convert main list to cards

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -5,8 +5,6 @@ import {
   IonContent,
   IonPage,
   IonLabel,
-  IonItem,
-  IonList,
   IonFooter,
   IonToolbar,
   IonIcon,
@@ -15,6 +13,10 @@ import {
   IonRow,
   IonCol,
   IonGrid,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
 } from "@ionic/react";
 
 import { home, bookmark, settings } from "ionicons/icons";
@@ -24,32 +26,120 @@ class MainMenu extends React.Component<{}, {}> {
     return (
       <IonPage>
         <IonContent>
-          <IonList>
-            <IonItem routerLink="/texts/louis-ck-01">
-              <IonLabel>Louis CK - 01</IonLabel>
-            </IonItem>
-            <IonItem routerLink="/texts/kaufmann-my-language-learning-technique">
-              <IonLabel>Kaufmann - My Language Learning Technique</IonLabel>
-            </IonItem>
-            <IonItem routerLink="/texts/hufflepuff-common-room">
-              <IonLabel>Hufflepuff Common Room</IonLabel>
-            </IonItem>
-            <IonItem routerLink="/texts/owls">
-              <IonLabel>Owls</IonLabel>
-            </IonItem>
-            <IonItem routerLink="/texts/daily-prophet">
-              <IonLabel>Daily Prophet</IonLabel>
-            </IonItem>
-            <IonItem routerLink="/texts/every-country-has-ninjas">
-              <IonLabel>Every Country Has Ninjas</IonLabel>
-            </IonItem>
-            <IonItem routerLink="/texts/platform-9">
-              <IonLabel>Platform 9 3/4</IonLabel>
-            </IonItem>
-            <IonItem routerLink="/texts/funny-dialogs">
-              <IonLabel>Funny Dialogs</IonLabel>
-            </IonItem>
-          </IonList>
+          <IonCard button routerLink="/texts/louis-ck-01">
+            <IonCardHeader>
+              <IonCardTitle>
+                <IonLabel>Louis CK - 01</IonLabel>
+              </IonCardTitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+              <p>Difficulty: Foo</p>
+              <p>Category: Bar</p>
+              <p>Length: Car</p>
+            </IonCardContent>
+          </IonCard>
+
+          <IonCard
+            button
+            routerLink="/texts/kaufmann-my-language-learning-technique"
+          >
+            <IonCardHeader>
+              <IonCardTitle>
+                <IonLabel>Kaufmann - My Language Learning Technique</IonLabel>
+              </IonCardTitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+              <p>Difficulty: Foo</p>
+              <p>Category: Bar</p>
+              <p>Length: Car</p>
+            </IonCardContent>
+          </IonCard>
+
+          <IonCard button routerLink="/texts/hufflepuff-common-room">
+            <IonCardHeader>
+              <IonCardTitle>
+                <IonLabel>Hufflepuff Common Room</IonLabel>
+              </IonCardTitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+              <p>Difficulty: Foo</p>
+              <p>Category: Bar</p>
+              <p>Length: Car</p>
+            </IonCardContent>
+          </IonCard>
+
+          <IonCard button routerLink="/texts/owls">
+            <IonCardHeader>
+              <IonCardTitle>
+                <IonLabel>Owls</IonLabel>
+              </IonCardTitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+              <p>Difficulty: Foo</p>
+              <p>Category: Bar</p>
+              <p>Length: Car</p>
+            </IonCardContent>
+          </IonCard>
+
+          <IonCard button routerLink="/texts/daily-prophet">
+            <IonCardHeader>
+              <IonCardTitle>
+                <IonLabel>Daily Prophet</IonLabel>
+              </IonCardTitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+              <p>Difficulty: Foo</p>
+              <p>Category: Bar</p>
+              <p>Length: Car</p>
+            </IonCardContent>
+          </IonCard>
+
+          <IonCard button routerLink="/texts/every-country-has-ninjas">
+            <IonCardHeader>
+              <IonCardTitle>
+                <IonLabel>Every Country Has Ninjas</IonLabel>
+              </IonCardTitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+              <p>Difficulty: Foo</p>
+              <p>Category: Bar</p>
+              <p>Length: Car</p>
+            </IonCardContent>
+          </IonCard>
+
+          <IonCard button routerLink="/texts/platform-9">
+            <IonCardHeader>
+              <IonCardTitle>
+                <IonLabel>Platform 9 3/4</IonLabel>
+              </IonCardTitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+              <p>Difficulty: Foo</p>
+              <p>Category: Bar</p>
+              <p>Length: Car</p>
+            </IonCardContent>
+          </IonCard>
+
+          <IonCard button routerLink="/texts/funny-dialogs">
+            <IonCardHeader>
+              <IonCardTitle>
+                <IonLabel>Funny Dialogs</IonLabel>
+              </IonCardTitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+              <p>Difficulty: Foo</p>
+              <p>Category: Bar</p>
+              <p>Length: Car</p>
+            </IonCardContent>
+          </IonCard>
         </IonContent>
         <IonFooter>
           <IonToolbar>
@@ -57,19 +147,31 @@ class MainMenu extends React.Component<{}, {}> {
               <IonGrid>
                 <IonRow>
                   <IonCol class="ion-text-center">
-                    <IonButton fill="clear">
-                      <IonIcon icon={home} size="large" />
-                    </IonButton>
+                    <div>
+                      <IonButton fill="clear">
+                        <IonIcon icon={home} size="large" />
+                      </IonButton>
+                      <br />
+                      Home
+                    </div>
                   </IonCol>
                   <IonCol class="ion-text-center">
-                    <IonButton fill="clear">
-                      <IonIcon icon={bookmark} size="large" />
-                    </IonButton>
+                    <div>
+                      <IonButton fill="clear">
+                        <IonIcon icon={bookmark} size="large" />
+                      </IonButton>
+                      <br />
+                      My Texts
+                    </div>
                   </IonCol>
                   <IonCol class="ion-text-center">
-                    <IonButton fill="clear">
-                      <IonIcon icon={settings} size="large" />
-                    </IonButton>
+                    <div>
+                      <IonButton fill="clear">
+                        <IonIcon icon={settings} size="large" />
+                      </IonButton>
+                      <br />
+                      Settings
+                    </div>
                   </IonCol>
                 </IonRow>
               </IonGrid>
