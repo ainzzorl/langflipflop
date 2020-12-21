72c233c97658c0eff3d8a93b599f53c25be8fbeb
ainzzorl <ainzzorl@gmail.com>
2020-12-20 19:02:02 -0800

    Add category filter

--- src/pages/LibraryMenu.tsx
+++ src/pages/LibraryMenu.tsx
@@ -8,6 +8,11 @@ import {
   IonCardHeader,
   IonCardTitle,
   IonCardContent,
+  IonPage,
+  IonList,
+  IonItem,
+  IonSelect,
+  IonSelectOption,
 } from "@ionic/react";
 
 class TextMeta {
@@ -24,11 +29,20 @@ class TextMeta {
   }
 }
 
-class LibraryMenu extends React.Component<{}, { texts?: Array<TextMeta> }> {
+class LibraryMenu extends React.Component<
+  {},
+  {
+    texts?: Array<TextMeta>;
+    allCategories: Array<String>;
+    categoryFilter?: string;
+  }
+> {
   constructor(props: any) {
     super(props);
     this.state = {
       texts: undefined,
+      categoryFilter: undefined,
+      allCategories: [],
     };
 
     fetch("assets/data/texts.json")
@@ -47,10 +61,26 @@ class LibraryMenu extends React.Component<{}, { texts?: Array<TextMeta> }> {
         let textMetas = textDatas.map((data) => {
           return new TextMeta(data);
         });
+        let allc = new Set<string>();
+        textMetas.forEach((textMeta) => {
+          textMeta.categories.forEach((c) => {
+            allc.add(c);
+          });
+        });
+        let allCategories = Array.from(allc).sort();
         this.setState((state) => ({
           texts: textMetas,
+          allCategories: allCategories,
         }));
       });
+
+    this.setCategoryFilter = this.setCategoryFilter.bind(this);
+  }
+
+  setCategoryFilter(value: string) {
+    this.setState((state) => ({
+      categoryFilter: value,
+    }));
   }
 
   render() {
@@ -62,28 +92,63 @@ class LibraryMenu extends React.Component<{}, { texts?: Array<TextMeta> }> {
       );
     }
 
-    let textCards = this.state.texts.map((textMeta, idx) => {
-      let routerLink = "/texts/" + textMeta.id;
-      return (
-        <li key={idx}>
-          <IonCard button routerLink={routerLink}>
-            <IonCardHeader>
-              <IonCardTitle>
-                <IonLabel>{textMeta.title}</IonLabel>
-              </IonCardTitle>
-            </IonCardHeader>
+    let textCards = this.state.texts
+      .filter((textMeta, _idx) => {
+        return (
+          !this.state.categoryFilter ||
+          textMeta.categories.includes(this.state.categoryFilter)
+        );
+      })
+      .map((textMeta, idx) => {
+        let routerLink = "/texts/" + textMeta.id;
+        return (
+          <li key={idx}>
+            <IonCard button routerLink={routerLink}>
+              <IonCardHeader>
+                <IonCardTitle>
+                  <IonLabel>{textMeta.title}</IonLabel>
+                </IonCardTitle>
+              </IonCardHeader>
 
-            <IonCardContent>
-              <p>Difficulty: {textMeta.difficulty}</p>
-              <p>Categories: {textMeta.categories.join(", ")}</p>
-              <p>Length: Car</p>
-            </IonCardContent>
-          </IonCard>
-        </li>
+              <IonCardContent>
+                <p>Difficulty: {textMeta.difficulty}</p>
+                <p>Categories: {textMeta.categories.join(", ")}</p>
+                <p>Length: Car</p>
+              </IonCardContent>
+            </IonCard>
+          </li>
+        );
+      });
+    let categoryOptions = this.state.allCategories.map((category, _idx) => {
+      return (
+        <IonSelectOption value={category} key={"selector-category-" + category}>
+          {category}
+        </IonSelectOption>
       );
     });
 
-    return <IonContent>{textCards}</IonContent>;
+    return (
+      <IonPage>
+        <IonContent>
+          <IonList>
+            <IonItem>
+              <IonLabel>Category</IonLabel>
+              <IonSelect
+                value={this.state.categoryFilter}
+                placeholder="Any"
+                onIonChange={(e) => this.setCategoryFilter(e.detail.value)}
+              >
+                <IonSelectOption value="" key={"selector-category-any"}>
+                  Any
+                </IonSelectOption>
+                {categoryOptions}
+              </IonSelect>
+            </IonItem>
+          </IonList>
+          {textCards}
+        </IonContent>
+      </IonPage>
+    );
   }
 }
 
