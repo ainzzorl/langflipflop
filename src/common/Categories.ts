d2eef90370b61be60a018910351a5b39e42ef198
ainzzorl <ainzzorl@gmail.com>
2021-01-01 17:23:42 -0800

    Add maradona-voanews

--- src/common/Categories.ts
+++ src/common/Categories.ts
@@ -2,7 +2,9 @@ export const CATEGORY_MAP: Map<string, string> = new Map([
   ["children", "Children's"],
   ["fairy-tale", "Fairy Tale"],
   ["adult", "Adult"],
+  ["sport", "Sport"],
+  ["article", "Article"],
 ]);
 
 // ORDERED list of categories.
-export const CATEGORIES = ["children", "fairy-tale", "adult"];
+export const CATEGORIES = ["children", "fairy-tale", "sport", "article"];
