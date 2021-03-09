e4ad10e30888a722b4c5f508177398ea36c8c12e
ainzzorl <ainzzorl@gmail.com>
2021-03-09 09:32:11 +0300

    Change "unregister" to "register" to fix SecurityError

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,4 @@ DAO.getSettings().then((settings) => {
   document.body.classList.toggle("dark", settings.theme === "dark");
 });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
