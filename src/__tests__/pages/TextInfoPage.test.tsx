0c2ae688b3ac67174d42405893a54e3fb03d99c4
ainzzorl <ainzzorl@gmail.com>
2021-03-29 16:29:11 +0300

    Fix test broken by description change

--- src/__tests__/pages/TextInfoPage.test.tsx
+++ src/__tests__/pages/TextInfoPage.test.tsx
@@ -6,7 +6,9 @@ test("Rendering Text Info", async () => {
   renderWithRoute("/text-infos/mysterious-monolith");
 
   await screen.findByText("Mysterious Metallic Monolith Found in Remote Utah");
-  await screen.findByText("Mysterious Metallic Monolith Found in Remote Utah.");
+  await screen.findByText(
+    "An article about a mysterious monolith found in remote Utah."
+  );
   await screen.findByText("Difficulty: Medium.");
   await screen.findByText("Categories: News.");
 });
