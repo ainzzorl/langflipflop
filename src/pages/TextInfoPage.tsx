dceb2946c3d8cace7a2cd479c382ebed8a107476
ainzzorl <ainzzorl@gmail.com>
2021-05-11 14:35:54 +0300

    Use lang1 to display stuff in TextInfoPage

--- src/pages/TextInfoPage.tsx
+++ src/pages/TextInfoPage.tsx
@@ -11,6 +11,7 @@ import {
 import React from "react";
 import { FormattedMessage } from "react-intl";
 import { RouteComponentProps } from "react-router-dom";
+import { getQueryParams } from "../common/Common";
 import TextMeta from "../common/TextMeta";
 
 interface TextInfoProps
@@ -23,23 +24,27 @@ class TextInfoPage extends React.Component<
   {
     text: any;
     meta: TextMeta;
+    lang1: string;
   }
 > {
   constructor(props: any) {
     super(props);
+
+    let params = getQueryParams(props);
     fetch(`assets/data/texts/${this.props.match.params.id}.json`)
       .then((res) => res.json())
       .then((res) => {
         this.setState(() => ({
           text: res,
           meta: new TextMeta(res),
+          lang1: (params["lang1"] as string) || "en",
         }));
       });
   }
 
   componentDidUpdate() {
     if (this.state && this.state.text) {
-      document.title = this.state.text["en"]["title"];
+      document.title = this.state.text[this.state.lang1]["title"];
     }
   }
 
@@ -61,13 +66,15 @@ class TextInfoPage extends React.Component<
                 defaultHref={`/texts/${this.props.match.params.id}${this.props.location.search}`}
               />
               <IonTitle color="medium">
-                <div className="my-wrap">{this.state.text["en"]["title"]}</div>
+                <div className="my-wrap">
+                  {this.state.text[this.state.lang1]["title"]}
+                </div>
               </IonTitle>
             </IonButtons>
           </IonToolbar>
         </IonHeader>
         <IonContent class="ion-padding">
-          <p>{this.state.text["en"]["description"]}</p>
+          <p>{this.state.text[this.state.lang1]["description"]}</p>
           <p>
             <IonText color="medium">
               <FormattedMessage id="text-info.difficulty" />
